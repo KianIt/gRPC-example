26390e4352bddc646365bda853419b1df6fff4df
KianIt <fursovnikita@yandex.ru>
2026-05-01 19:15:30 +0300

    Added client-server interaction example

---
+++ cmd/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	pbService "github.com/KianIt/gRPC-example/protobuf/service"
+)
+
+var (
+	address      = ":49000"
+	messageCount = 10
+)
+
+func main() {
+	log.Printf("Starting gRPC server")
+
+	if err := startServer(address); err != nil {
+		log.Fatalf("Error starting gRPC server: %s", err)
+	}
+
+	log.Printf("Setting up gRPC client")
+
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Error creating connection: %v", err)
+	}
+	defer func() { _ = conn.Close() }()
+
+	service := pbService.NewServiceClient(conn)
+
+	log.Printf("[CLIENT] Sending messages to %s", address)
+
+	for i := 1; i <= messageCount; i++ {
+		message := &pbService.Message{}
+		message.SetId(int32(i))
+		if i%2 == 0 {
+			message.SetType(pbService.MessageType_MESSAGE_TYPE_ALERT)
+		}
+		message.SetTitle("title")
+		message.SetTags([]string{"tag1", "tag2"})
+		message.SetData([]byte("data"))
+		if i == messageCount {
+			message.SetIsLast(true)
+		} else {
+			message.SetCountMore(int32(messageCount - i))
+		}
+		message.SetSentAt(timestamppb.Now())
+
+		log.Printf("[CLIENT] Sending message %d: %v", i, message)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(i+1)*time.Second)
+
+		if _, err = service.ProcessMessage(ctx, message); err != nil {
+			log.Printf("[CLIENT] Error processing message: %v", err)
+		} else {
+			log.Printf("[CLIENT] Message successfully processed")
+		}
+
+		cancel()
+	}
+}
+
+type serviceImplement struct {
+	pbService.UnimplementedServiceServer
+}
+
+func (s *serviceImplement) ProcessMessage(_ context.Context, message *pbService.Message) (*emptypb.Empty, error) {
+	if message.GetType() == pbService.MessageType_MESSAGE_TYPE_INFO {
+		log.Printf("[SERVER] Received INFO message %d (ts: %v)", message.GetId(), message.GetSentAt().AsTime())
+	} else {
+		log.Printf("[SERVER] Received ALERT message %d (ts: %v)", message.GetId(), message.GetSentAt().AsTime())
+	}
+
+	log.Printf("[SERVER] Message '%s': %s", message.GetTitle(), string(message.GetData()))
+
+	if message.GetIsLast() {
+		log.Printf("[SERVER] No more messages expected")
+	} else {
+		log.Printf("[SERVER] Expecting %d more messages", message.GetCountMore())
+	}
+
+	log.Println("[SERVER] Processing message")
+	time.Sleep(time.Duration(message.GetId()) * time.Second)
+
+	return &emptypb.Empty{}, nil
+}
+
+func startServer(address string) error {
+	linstener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("tcp linstener: %w", err)
+	}
+
+	server := grpc.NewServer()
+
+	pbService.RegisterServiceServer(server, &serviceImplement{})
+
+	log.Printf("[SERVER] Server listening at: %s", address)
+
+	go func() {
+		if err = server.Serve(linstener); err != nil {
+			log.Fatalf("[SERVER] Serving error: %v", err)
+		}
+	}()
+
+	return nil
+}
